8f955d8d5098e96c5be08ae5fe7eeebda36f2a3f
Ilya Izotov <normall777@yandex.ru>
2018-12-25 22:32:50 +0500

    Start
    Создан проект, добавлена сущность UserClass

---
+++ WebApplicationUsers/WebApplicationUsers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationUsers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplicationUsers/WebApplicationUsers/Models/UserClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationUsers.Models
+{
+    public class UserClass : IdentityUser
+    {
+        public string Name { set; get; }
+        public string Surname { set; get; }
+        public string Patronymic { set; get; }
+
+    }
+}
