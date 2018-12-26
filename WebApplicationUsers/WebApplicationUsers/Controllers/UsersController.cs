6da3edf6004620af08c16842c333184c8ea6eb2e
Ilya Izotov <normall777@yandex.ru>
2018-12-26 16:05:28 +0500

    Step 10 done
    Добавлен контроллер (UsersController)
    В _Layout добавленна ссылка на представление Index для UsersController
    UsersController необходимо дописать (шаг 12) и разобраться со страницей регистрации
    При создании представления Edit (шаг 13) модель почему-то не отображается в списке

---
+++ WebApplicationUsers/WebApplicationUsers/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationUsers.Data;
+using WebApplicationUsers.Models;
+
+namespace WebApplicationUsers.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Переход на страницу редактирования/регистрации пользователя
+        /// </summary>
+        [HttpGet]
+        public IActionResult Edit(string id, [FromServices] ApplicationDbContext db)
+        {
+            //TODO:
+
+            return View("Index");
+        }
+        /// <summary>
+        /// Процедура регистрации/редактирования пользователя с сохранением
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Edit(ApplicationUser user,
+            [FromServices] ApplicationDbContext db,
+            [FromServices] UserManager<ApplicationUser> userManager)
+        {
+            //TODO:
+            return View("Index");
+        }
+
+        public async Task<IActionResult> Remove(string id,
+            [FromServices] UserManager<ApplicationUser> userManager)
+        {
+            //TODO:
+            return View("Index");
+        }
+
+
+    }
+}
