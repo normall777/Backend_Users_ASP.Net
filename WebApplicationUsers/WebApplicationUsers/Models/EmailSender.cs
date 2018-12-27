f3a83d18871a1d76bba456c71ec51887f2b7a603
Rafnsvart <rafnsvart.d@yandex.ru>
2018-12-27 23:07:47 +0500

    Добавил почту

--- WebApplicationUsers/WebApplicationUsers/Models/EmailSender.cs
+++ WebApplicationUsers/WebApplicationUsers/Models/EmailSender.cs
@@ -14,7 +14,7 @@ namespace WebApplicationUsers.Models
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "login@yandex.ru"));
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "rafnsvart.t@gmail.com"));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -24,8 +24,9 @@ namespace WebApplicationUsers.Models
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync("smtp.yandex.ru", 25, false);
-                await client.AuthenticateAsync("login@yandex.ru", "password");
+                //await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                await client.ConnectAsync("smtp.gmail.com", 25, false);
+                await client.AuthenticateAsync("rafnsvart.t@gmail.com", "yfnnafhb96");
                 await client.SendAsync(emailMessage);
 
                 await client.DisconnectAsync(true);
