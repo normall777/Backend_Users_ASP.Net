f37116fb79eafed59e880b531323285a85565da4
Ilya Izotov <normall777@yandex.ru>
2018-12-28 11:39:40 +0500

    Изначальная инициализация администратора
    При первом включении будет создан администратор 1@2.3 123456

---
+++ WebApplicationUsers/WebApplicationUsers/Models/AddInBD.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationUsers.Models
+{
+    public class AddInBD
+    {
+        public static async Task InitializeAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "1@2.3";
+            string password = "123456";
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                ApplicationUser admin = new ApplicationUser { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
--- WebApplicationUsers/WebApplicationUsers/Program.cs
+++ WebApplicationUsers/WebApplicationUsers/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApplicationUsers.Models;
 
 namespace WebApplicationUsers
 {
@@ -14,7 +17,29 @@ namespace WebApplicationUsers
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+            
+            //Инициализация главного администратора
+            using (var scope = host.Services.CreateScope())
+            {
+                Task t;
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    t = AddInBD.InitializeAsync(userManager, rolesManager);
+                    t.Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
